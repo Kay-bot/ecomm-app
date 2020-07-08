f6c88de7222a728dce28969caf2b241e95db86b9
KAY S <KS.KWANRUAN@GMAIL.com>
2020-07-09 09:51:46 +1000

    redirect

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { handleErrors } = require("./middlewares");
 
+const { handleErrors } = require("./middlewares");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -28,7 +28,7 @@ router.post(
 
     req.session.userId = user.id;
 
-    res.send("Account created!!!");
+    res.redirect("/admin/products");
   }
 );
 
@@ -52,7 +52,7 @@ router.post(
 
     req.session.userId = user.id;
 
-    res.send("You are signed in!!!");
+    res.redirect("/admin/products");
   }
 );
 
