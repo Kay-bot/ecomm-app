9ce7f64484776f63e657f355a286fb8bb8b6823d
KAY S <KS.KWANRUAN@GMAIL.com>
2020-07-09 12:13:01 +1000

    product router added

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
-const productsRouter = require("./routes/admin/products");
-
+const AdminProductsRouter = require("./routes/admin/products");
+const productsRouter = require("./routes/products");
 const app = express();
 
 app.use(express.static("public"));
@@ -14,6 +14,7 @@ app.use(
   })
 );
 app.use(authRouter);
+app.use(AdminProductsRouter);
 app.use(productsRouter);
 
 app.listen(3000, () => {
